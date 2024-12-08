237be485bf221e95a9fe0acf3c2065d659af8bfa
orzoz <orzozx@gmail.com>
2024-12-08 22:34:11 +0800

    feat: add git diff checks in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    // first check if there are any cached changes in the working tree
+    let output = Command::new("git")
+        .arg("diff")
+        .arg("--cached")
+        .output()
+        .expect("please install git");
+    if !output.status.success() {
+        eprintln!("error: git diff --cached failed");
+        return;
+    }
+    let mut stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    if stdout.is_empty() {
+        // if there are no cached changes, check for unstaged changes
+        let output = Command::new("git")
+            .arg("diff")
+            .output()
+            .expect("please install git");
+        if !output.status.success() {
+            eprintln!("error: git diff failed");
+            return;
+        }
+        stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    }
+    println!("{}", stdout);
 }
